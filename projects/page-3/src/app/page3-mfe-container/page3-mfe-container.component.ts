6c18e934f15a99e632e17d204db1f2be15ebb8f5
Rajarajan Packianathan <rckraja@hotmail.com>
2025-02-24 12:40:43 +0530

    third commit

--- projects/page-3/src/app/page3-mfe-container/page3-mfe-container.component.ts
+++ projects/page-3/src/app/page3-mfe-container/page3-mfe-container.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page3-mfe-container',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './page3-mfe-container.component.html',
-  styleUrl: './page3-mfe-container.component.css'
+  styleUrl: './page3-mfe-container.component.css',
 })
 export class Page3MfeContainerComponent {
+  public activeTab: string = 'inventory';
 
+  openTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
